dea20a3633017d4081c400290cbe31b67890cce4
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-11-07 10:06:34 -0600

    Set up fetch, useState, useEffect, and conditional rendering.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect } from 'react';
+import Loading from './Loading';
+import Tours from './Tours';
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = 'https://course-api.com/react-tours-project';
+
 function App() {
-  return <h2>Tours Project Setup</h2>
+  // Set up states
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  // Fetch tour data
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      // confirm we're getting that data
+      console.log(tours);
+      // update our state variables
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  // use useEffect() to trigger the fetch once on the page load
+  useEffect(
+    () => {
+      // invoke the fetch function
+      fetchTours();
+    },
+    // once
+    []
+  );
+
+  // Apply conditional rendering to use a spinner while the data is fetched
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours />
+    </main>
+  );
 }
 
-export default App
+export default App;

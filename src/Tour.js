07b327912c93793a919d40f01347b57efd6f8658
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-11-07 10:41:22 -0600

    Use state and conditional rendering to handle read more toggle.

--- src/Tour.js
+++ src/Tour.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 
 // grab all the info being passed down from Tours
 const Tour = ({ id, image, info, price, name }) => {
+  // create a state variable to toggle the read more
+  const [readMore, setReadMore] = useState(false);
   // enhance the data that is being mapped into each component
   return (
     <article className="single-tour">
@@ -11,7 +13,13 @@ const Tour = ({ id, image, info, price, name }) => {
           <h4>{name}</h4>
           <h4 className="tour-price">${price}</h4>
         </div>
-        <p>{info}</p>
+        {/* use conditional rendering to toggle between read more states */}
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? `read less` : `read more`}
+          </button>
+        </p>
         <button className="delete-btn">not interested</button>
       </footer>
     </article>
